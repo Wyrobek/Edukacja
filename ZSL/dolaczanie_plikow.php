20790f0928719bed3b094fdd52e8e027ea049c7e
Wyrobek <89968954+Wyrobek@users.noreply.github.com>
2021-09-16 10:44:23 +0200

    Add files via upload

---
+++ ZSL/dolaczanie_plikow.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>Dołączenie pliku 3_1.php</title>
+</head>
+<body>
+
+<h3>Początek pliku</h3>
+    
+    <?php
+
+        
+        include '3_1.php';
+        require '3_1.php';
+        
+
+    ?>
+
+<h3>Koniec pliku</h3>
+
+</body>
+</html>
